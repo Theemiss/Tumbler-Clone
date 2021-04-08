839f09b5fe182dcfae4883c05e05c3a9a5b2d818
Themis <midinfotn401@gmail.com>
2021-04-08 22:37:45 +0100

    implemented delete and modefy for post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,6 @@
 class PostsController < ApplicationController
     def index
-        @posts = Post.all.order('created_at')
+        @posts = Post.all.order('created_at DESC')
     end
     def new
         @post = Post.new
@@ -17,10 +17,33 @@ class PostsController < ApplicationController
 
 
     end
+
     def show
         @post = Post.find(params[:id])
     end
+    def edit
+        @post = Post.find(params[:id])
 
+    end
+
+    def update
+        @post = Post.find(params[:id])
+
+        if @post.update(params[:post].permit(:title, :body))
+            redirect_to @post
+        else
+            render 'edit'
+        end
+
+    end
+
+    def destroy
+        @post = Post.find(params[:id])
+        @post.destroy
+        
+        redirect_to root_path
+
+    end
     private
 
     def post_params
